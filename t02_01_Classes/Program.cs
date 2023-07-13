0206eff4d46a0592a7172dfe5791db9c939cceaa
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-07-13 14:40:38 +0200

    Classes

--- t02_01_Classes/Program.cs
+++ t02_01_Classes/Program.cs
@@ -1,10 +1,37 @@
 ﻿namespace t02_01_Classes
 {
+    internal class Person
+    {
+        internal string Name;
+
+        internal void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}.", to, Name);
+        }
+
+        internal static Person Parse(string str)
+        {
+            Person person = new();
+            person.Name = str;
+
+            return person;
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Person jugoslav = new();
+            jugoslav.Name = "Jugoslav";
+
+            var mosh = new Person();
+            mosh.Name = "Mosh";
+
+            jugoslav.Introduce(mosh.Name);
+
+            var perica = Person.Parse("Perica");
+            jugoslav.Introduce(perica.Name);
         }
     }
 }
