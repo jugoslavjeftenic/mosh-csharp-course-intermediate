700830894ad22b3f5cd2493cae9012bbe5a38229
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-07-13 14:08:17 +0200

    Classes

--- t02_01_Classes/Program.cs
+++ t02_01_Classes/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace t02_01_Classes
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
