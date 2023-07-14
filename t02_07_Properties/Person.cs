dafb20770ee6995863ee6e7788db697d922b5673
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-07-14 12:26:14 +0200

    Properties

---
+++ t02_07_Properties/Person.cs
@@ -0,0 +1,25 @@
+﻿namespace t02_07_Properties
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public DateTime Birthdate { get; private set; }
+
+        public Person(DateTime birthdate)
+        {
+            Birthdate = birthdate;
+        }
+
+        public int Age
+        {
+            get
+            {
+                var timeSpan = DateTime.Today - Birthdate;
+                var years = timeSpan.Days / 365;
+
+                return years;
+            }
+        }
+    }
+}
---
+++ t02_07_Properties/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace t02_07_Properties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person(new DateTime(1972, 9, 16));
+
+            Console.WriteLine(person.Age);
+        }
+    }
+}
