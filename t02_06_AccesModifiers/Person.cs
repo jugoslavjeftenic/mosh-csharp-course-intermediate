e0253bb06665b6a8bb16be6a35b4b3e83db5e408
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-07-14 11:36:55 +0200

    AccesModifiers

---
+++ t02_06_AccesModifiers/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace t02_06_AccesModifiers
+{
+    internal class Person
+    {
+        private DateTime _birthdate;
+
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        }
+    }
+}
---
+++ t02_06_AccesModifiers/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace t02_06_AccesModifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+
+            person.SetBirthdate(new DateTime(1972, 9, 16));
+
+            Console.WriteLine(person.GetBirthdate());
+        }
+    }
+}
