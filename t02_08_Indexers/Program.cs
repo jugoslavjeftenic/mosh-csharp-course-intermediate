bbe93cb6e2d9ea67ba9a291fc3b7415bd5cffa3f
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-07-14 12:48:36 +0200

    Indexers

---
+++ t02_08_Indexers/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace t02_08_Indexers
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var cookie = new HttpCookie();
+
+            cookie["name"] = "Jugoslav";
+
+            Console.WriteLine(cookie["name"]);
+        }
+    }
+}
