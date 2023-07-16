48ef2cc90ad00eff11a753a0c687baff460a6862
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-07-16 13:00:06 +0200

    Inheritance

---
+++ t03_02_Inheritance/PresentationObject.cs
@@ -0,0 +1,18 @@
+﻿namespace t03_02_Inheritance
+{
+    internal class PresentationObject
+    {
+        internal int Width { get; set; }
+        internal int Height { get; set; }
+
+        internal void Copy()
+        {
+            Console.WriteLine("Object copied to clipboard.");
+        }
+
+        internal void Duplicate()
+        {
+            Console.WriteLine("Object was duplicated.");
+        }
+    }
+}
---
+++ t03_02_Inheritance/Text.cs
@@ -0,0 +1,13 @@
+﻿namespace t03_02_Inheritance
+{
+    internal class Text : PresentationObject
+    {
+        internal int FontSize { get; set; }
+        internal string? FontName { get; set; }
+
+        internal void AddHyperLink(string url)
+        {
+            Console.WriteLine($"We added a link to {url}");
+        }
+    }
+}
