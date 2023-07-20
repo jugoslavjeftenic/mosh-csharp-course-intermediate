3e9bbd11811467518b8a8648c5d02d78cb21c552
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-07-20 09:30:13 +0200

    Boxing

---
+++ t04_04_Boxing/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+namespace t04_04_Boxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Boxing(performance penalty) / not type safe
+            var list = new ArrayList();
+            list.Add(1);
+            list.Add("Jugoslav");
+            list.Add(DateTime.Today);
+
+            // No Boxing / type safe
+            var anotherList = new List<int>();
+            anotherList.Add(1);
+
+            var names = new List<string>();
+            names.Add("Jugoslav");
+        }
+    }
+}
